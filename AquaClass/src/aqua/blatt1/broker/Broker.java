f407e89efb328c394b278e3d53bc7ba318c137ed
sivan <sascha.ivan99@gmail.com>
2020-10-15 17:16:12 +0200

    add methods to Broker class

--- AquaClass/src/aqua/blatt1/broker/Broker.java
+++ AquaClass/src/aqua/blatt1/broker/Broker.java
@@ -1,41 +1,59 @@
 package aqua.blatt1.broker;
 
 import aqua.blatt1.client.ClientCommunicator;
+import aqua.blatt1.common.Properties;
+import aqua.blatt1.common.msgtypes.DeregisterRequest;
+import aqua.blatt1.common.msgtypes.HandoffRequest;
 import aqua.blatt1.common.msgtypes.RegisterRequest;
 import aqua.blatt1.common.msgtypes.RegisterResponse;
 import messaging.Endpoint;
 import messaging.Message;
-
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 public class Broker {
-    private ClientCollection<ClientCommunicator> clients = new ClientCollection<>();
+    private ClientCollection<ClientCommunicator> clients;
 
     private Endpoint endpoint;
 
     public Broker() {
-        this.endpoint = new Endpoint(4711);
-        while(true) {
-            Message message = this.endpoint.blockingReceive();
-            var payload = message.getPayload();
+        this.endpoint = new Endpoint(Properties.PORT);
+        this.clients = new ClientCollection<>();
+    }
 
+    private void broker() {
+        while(true) {
+            var message = this.endpoint.blockingReceive();
+            var payload = message.getPayload();
+            if (payload instanceof RegisterRequest)
+                register(message.getSender());
+
+            if (payload instanceof DeregisterRequest)
+                deregister(this.clients.indexOf(message.getSender().getHostName()));
+
+            if (payload instanceof HandoffRequest)
+                handoffFisch();
         }
     }
 
     private void register(InetSocketAddress addr) {
         ClientCommunicator newClient = new ClientCommunicator();
-        String id = "tank" + this.clients.size() + 1;
+        var id = "tank" + this.clients.size() + 1;
         this.clients.add("tank" + this.clients.size() + 1, newClient);
         this.endpoint.send(addr, new RegisterResponse(id));
     }
 
-    private void deregister() {
+    private void deregister(int id) {
+        this.clients.remove(id);
+    }
+
+    private void handoffFisch() {
 
     }
 
     public static void main(final String[] args) {
         Broker broker = new Broker();
+        broker.broker();
     }
 }
 
