bd1aafcd083183a9dd7b8167d8220041313f0017
saschaivan <sascha.ivan99@gmail.com>
2020-11-04 15:11:06 +0100

    finished Blatt2

--- AquaClass/src/aqua/blatt1/broker/Broker.java
+++ AquaClass/src/aqua/blatt1/broker/Broker.java
@@ -1,5 +1,7 @@
 package aqua.blatt1.broker;
 
+import aqua.blatt1.aqua.blatt2.broker.PoisonPill;
+import aqua.blatt1.aqua.blatt2.broker.Poisoner;
 import aqua.blatt1.client.ClientCommunicator;
 import aqua.blatt1.common.Direction;
 import aqua.blatt1.common.FishModel;
@@ -22,13 +24,11 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class Broker {
     private ClientCollection<InetSocketAddress> clients;
     private Endpoint endpoint;
-    private int numThreads = 10;
+    private int numThreads = 6;
     private int counter;
     private volatile boolean stopRequested = false;
-    //private Poisoner poisoner;
 
     ExecutorService executerService;
-    ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public Broker() {
         endpoint = new Endpoint(Properties.PORT);
@@ -37,14 +37,31 @@ public class Broker {
     }
 
     private void broker() {
+        Thread stopServerThread = new Thread(() -> {
+            int a = JOptionPane.showOptionDialog(null,
+                    "Press ok to stop Server",
+                    "",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    null,
+                    null);
+            if (a == 0) {
+                stopRequested = true;
+                System.exit(0);
+            }
+        });
+        stopServerThread.start();
         if (!stopRequested) {
             while(true) {
-                var brokerTask = new BrokerTask();
+                var message = endpoint.blockingReceive();
+                var brokerTask = new BrokerTask(message);
                 executerService.execute(brokerTask);
             }
         }
+        System.exit(0);
         executerService.shutdown();
     }
+
 
     public static void main(final String[] args) {
         Broker broker = new Broker();
@@ -52,27 +69,34 @@ public class Broker {
     }
 
     public class BrokerTask implements Runnable {
+        ReadWriteLock lock;
+        Message message;
+
+        public BrokerTask(Message message) {
+            lock = new ReentrantReadWriteLock();
+            this.message = message;
+        }
+
         @Override
         public void run() {
-            var message = endpoint.blockingReceive();
             var payload = message.getPayload();
             if (payload instanceof RegisterRequest)
                 register(message);
-
             if (payload instanceof DeregisterRequest)
                 deregister(message);
-
             if (payload instanceof HandoffRequest)
                 handoffFish(message);
+            if (payload instanceof PoisonPill)
+                stopRequested = true;
         }
 
         private void register(Message message) {
-            lock.writeLock().lock();
             var id = "tank" + counter++;
             var sender = message.getSender();
+            lock.writeLock().lock();
             clients.add(id, sender);
-            endpoint.send(sender, new RegisterResponse(id));
             lock.writeLock().unlock();
+            endpoint.send(sender, new RegisterResponse(id));
         }
 
         private void deregister(Message message) {
@@ -85,18 +109,18 @@ public class Broker {
         }
 
         private void handoffFish(Message message) {
-            lock.readLock().lock();
             var handoffRequest = (HandoffRequest) message.getPayload();
             InetSocketAddress receiver;
             FishModel fish = handoffRequest.getFish();
+            lock.readLock().lock();
             var tankindex = clients.indexOf(message.getSender());
+            lock.readLock().unlock();
             if (fish.getDirection() == Direction.LEFT)
                 receiver = clients.getLeftNeighorOf(tankindex);
             else
                 receiver = clients.getRightNeighorOf(tankindex);
 
             endpoint.send(receiver, handoffRequest);
-            lock.readLock().unlock();
         }
     }
 }
